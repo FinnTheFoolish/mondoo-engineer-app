85592fa44944abbdb40cc54d5c0132b690f96722
fb354 <fb354@kent.ac.uk>
2026-09-19 17:46:57 +0100

    Add HTTP health-check server

---
+++ cmd/healthcheck/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+const healthURL = "http://127.0.0.1:8080/health"
+
+func main() {
+	if err := checkHealth(healthURL); err != nil {
+		os.Exit(1)
+	}
+}
+
+func checkHealth(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("health check returned HTTP status %d", resp.StatusCode)
+	}
+
+	return nil
+}
